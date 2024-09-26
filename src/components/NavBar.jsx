c034a3c336ddc5395dec87e56ceb64f775e6f228
Ezequiel Blanco <rodriezequiel2000@hotmail.com>
2024-09-25 21:03:16 -0300

    Preentrega1Blanco

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <header className="header"> 
+<a href="/" className="logo">Eze</a>
+<nav className="navbar">
+<a href="/">inicio</a>
+<a href="/">categorias</a>
+<a href="/">servicios</a>
+<a href="/">ingresar</a>
+
+
+</nav>
+
+
+
+
+
+
+        </header>
+
+    )
+}
+
+export default NavBar
